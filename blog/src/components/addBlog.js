6bd5078fac0e3762b93c2b99f68e784d455ff14f
Nithin Migo <nithinmigo1@gmail.com>
2020-12-31 20:01:56 +0530

    api call

--- blog/src/components/addBlog.js
+++ blog/src/components/addBlog.js
@@ -12,7 +12,7 @@ function AddBlog() {
     }, [])
 
     const fetchAll = () =>{
-        axios.get('/api')
+        axios.get('http://localhost:5000/api')
         .then((res) => {
             setListdata(res.data);
         })
