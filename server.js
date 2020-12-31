c1552eda6d8b5f40de2cd0874b9f543e089c56e5
Nithin Migo <nithinmigo1@gmail.com>
2020-12-31 19:30:30 +0530

    setted up heroku client

--- server.js
+++ server.js
@@ -33,6 +33,10 @@ app.use('/',route);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('blog/build'));
+
+    app.get('*',(req,res) => {
+        res.sendFile(path.join(__dirname , 'blog','build','index.html'));
+    })
 }
 
 app.listen(PORT,console.log(`Server started at port ${PORT}`))
