63948df64c57faa3eb1cbbee2f0a8ba9cd31a6b5
Nithin Migo <nithinmigo1@gmail.com>
2020-12-31 16:52:33 +0530

    remove mongo live check

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT||8080
 
 const MONGO_URL = 'mongodb+srv://nithinmigo:migonithin@cluster0.jtqoc.mongodb.net/<dbname>?retryWrites=true&w=majority';
 
-mongoose.connect(process.env.MONGO_URL || 'mongodb://localhost/mern_1',{
+mongoose.connect(process.env.MONGO_URL,{
     useNewUrlParser : true,
     useUnifiedTopology : true
 });
