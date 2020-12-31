e376ca1bae40d6b43155540caba7e6abad6f45f2
Nithin Migo <nithinmigo1@gmail.com>
2020-12-31 20:11:58 +0530

    update

--- server.js
+++ server.js
@@ -34,12 +34,12 @@ app.use(cors());
 
 app.use('/api',route);
 
-// if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('blog/build'));
 
     app.get('*',(req,res) => {
         res.sendFile(path.join(__dirname ,'blog','build','index.html'));
     })
-//}
+}
 
 app.listen(PORT,console.log(`Server started at port ${PORT}`))
