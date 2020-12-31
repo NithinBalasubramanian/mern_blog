722116580ba4f067ba9256483c40481e0ce1c87a
Nithin Migo <nithinmigo1@gmail.com>
2020-12-31 19:33:13 +0530

    setted up heroku client

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ if(process.env.NODE_ENV === 'production'){
     app.use(express.static('blog/build'));
 
     app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname , 'blog','build','index.html'));
+        res.sendFile(path.join(__dirname ,'blog','build','index.html'));
     })
 }
 
