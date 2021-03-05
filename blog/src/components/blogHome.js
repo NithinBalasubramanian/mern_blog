e67c08dd784114bee0fcca22e5642d0736d531be
Nithin <nithinmigocrogle22@outlook.com>
2021-03-05 13:00:13 +0530

    Update blogHome.js

--- blog/src/components/blogHome.js
+++ blog/src/components/blogHome.js
@@ -31,7 +31,7 @@ function BlogHome() {
                     <div className="col-md-6 card_col" >
                         <div className="card_home">
                             <Link to={'/Blog/'+itm.url} exact >
-                                <img src={itm.imgUrl} alt="img" width="100%" height="250px" /> 
+                                <img src={itm.imgUrl} alt={itm.title} width="100%" height="250px" /> 
                                 <h4>{itm.title}</h4>
                                 <div className="byAuth">
                                     - by {itm.auther} 
@@ -46,7 +46,7 @@ function BlogHome() {
                     <div className="col-md-4 card_col">
                         <div className="card_home">
                             <Link to={'/Blog/'+itm.url} exact >
-                                <img src={itm.imgUrl} alt="img" width="100%" height="250px" /> 
+                                <img src={itm.imgUrl} alt={itm.title} width="100%" height="250px" /> 
                                 <h4>{itm.title}</h4>
                                 <div className="byAuth">
                                     - by {itm.auther} 
