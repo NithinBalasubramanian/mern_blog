262e5e6dd427666b12188cdfc20b2195a5a59bb9
Nithin <nithinmigocrogle22@outlook.com>
2021-03-06 15:57:36 +0530

    Update blogHome.js

--- blog/src/components/blogHome.js
+++ blog/src/components/blogHome.js
@@ -36,12 +36,12 @@ function BlogHome() {
                         <div className="card_home">
                             <Link to={'/Blog/'+itm.url} exact >
                                 <img src={itm.imgUrl} alt={itm.title} width="100%" height="250px" /> 
-                                <h4>{itm.title}</h4>
-                                <p className="frontSubCont">{itm.preheading}</p>
                                 <div className="byAuth">
                                     - by {itm.auther} 
                                     <small>{moment(itm.createdOn).fromNow()}</small>
                                 </div>
+                                <h4>{itm.title}</h4>
+                                <p className="frontSubCont">{itm.preheading}</p>
                             </Link>
                         </div>                        
                     </div>
