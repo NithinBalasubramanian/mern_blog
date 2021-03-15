b25a4a71ea362e18244d35707c612bb60c7749e3
Nithin <nithinmigocrogle22@outlook.com>
2021-03-15 17:19:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -22,6 +22,12 @@ mongoose.connection.on('connected',()=>{
     console.log('connected db');
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(fileupload());
 
 app.use(express.json());
