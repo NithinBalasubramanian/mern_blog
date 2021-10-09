03e7bebd249b397068dd8172a52ea2cacc088b1f
Nithin <nithinmigocrogle22@outlook.com>
2021-10-09 08:57:38 +0530

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -165,6 +165,19 @@ router.delete('/deletelink/:id',(req,res) => {
 
 //Api fetch for tech-Idiots ( Home Page )
 
+
+
+router.get('/homeBlogs',(req,res) => {
+    testBlog.find({}).sort({createdOn:1})
+    .then((data) => { 
+        // console.log('data',data);
+        res.json(data);
+    })
+    .catch((error) => { 
+        console.log('error',error);
+    })
+});
+
 router.get('/homeTopFetch',(req,res) => {
     testBlog.find({}).sort({createdOn:-1}).limit(8)
     .then((data) => { 
