53eb4338b53fc4dec89754d38b38a88cd351f4f1
Nithin <nithinmigocrogle22@outlook.com>
2022-01-09 22:44:48 +0530

    Create NewBlog.js

---
+++ blog/src/components/NewBlog.js
@@ -0,0 +1,42 @@
+import React, { useState , useEffect } from 'react';
+
+import parse from 'html-react-parser';
+import axios from '../config/instance';
+
+const Newblog = () => { 
+
+let [ blog , setBlog ] = useState('')
+
+useEffect(()=>{
+    getBlog()
+},[])
+
+const getBlog = () => {
+
+   axios.post('newBlog')
+        .then(res=>{
+                 // setDatas(initialState);
+             setBlog(res.data)
+        })
+        .catch(err=>{
+            console.log(err);
+        })
+      }
+}
+
+
+return( <>
+<div> 
+       { blog.map((itm)=>{
+        itm.blogData && 
+           <div>
+                { parse(itm.blogData) }
+            </div>
+            </hr>
+        })
+           
+         </div>
+
+        </>
+
+   }
