3cda8e975fd892463e497148583b2ff8b2fb62a4
nithin balasubramanian <nihtinfurie17@gmail.com>
2024-05-07 22:48:57 +0530

    feaT: revert

--- server.js
+++ server.js
@@ -44,11 +44,11 @@ app.use(cors());
 app.use('/api',route);
 
 // if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('blog/build'));
+//     app.use(express.static('blog/build'));
 
-    app.get('*',(req,res) => {
-        res.sendFile(path.join(__dirname ,'blog','build','index.html'));
-    })
+//     app.get('*',(req,res) => {
+//         res.sendFile(path.join(__dirname ,'blog','build','index.html'));
+//     })
 // }
 
 app.listen(PORT,console.log(`Server started at port - v2 ${PORT}`))
